9c32c4da52ce130ad23dcb2325c92ad48cbecc5e
rahulverman121 <98263712+rahulverman121@users.noreply.github.com>
2022-03-27 02:34:50 +0530

    Update home.php

--- home.php
+++ home.php
@@ -7,6 +7,7 @@ session_start();
 <head>
 <title>WooTube | Movie Library | All your favourite movies at one place
 </title>
+<link rel="icon" type="image/jpg" href="w.jpg" >
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
